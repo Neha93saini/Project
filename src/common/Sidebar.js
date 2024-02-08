f500e4718bf6d39ad9eb2d589560baafa5dd12ab
neha93saini <neha93saini@gmail.com>
2024-02-08 23:51:15 +0530

    side bar changes as per github pages

--- src/common/Sidebar.js
+++ src/common/Sidebar.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 const Sidebar = () => {
     return (
         <div className="sidebar">
             <Navbar bg="dark" variant="dark" expand="md" fixed="left">
                 <Navbar.Collapse >
                     <Nav className="flex-column">
-                        <Nav.Link href="/">Dashboard</Nav.Link>
-                        <Nav.Link href="/form">Form</Nav.Link>
-                        <Nav.Link href="/table">Table</Nav.Link>
+                        <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
+                        <Nav.Link as={Link} to="/form">Form</Nav.Link>
+                        <Nav.Link as={Link} to="/table">Table</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
